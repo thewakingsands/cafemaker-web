f51efc1f3cba1bc1eca5ad647bf82dc0bc14b44f
oott123 <git@public.oott123.com>
2019-07-25 23:16:09 +0800

    chroe: update scripts

--- vm/Database.sql
+++ vm/Database.sql
@@ -1,9 +1,3 @@
-CREATE DATABASE dalamud;
-CREATE USER dalamud@localhost IDENTIFIED BY 'dalamud';
-GRANT ALL PRIVILEGES ON *.* TO dalamud@'%' IDENTIFIED BY 'dalamud';
-GRANT ALL PRIVILEGES ON *.* TO dalamud@localhost IDENTIFIED BY 'dalamud';
-FLUSH PRIVILEGES;
-
 -- Adminer 4.6.3 MySQL dump
 
 SET NAMES utf8;
